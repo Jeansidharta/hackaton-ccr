ab7e0ee217bf6a2fb600e00e1c9cc95d0b7dcaf9
jean sidharta <jeansidharta@gmail.com>
2021-01-17 21:05:29 -0300

    Created admin page

---
+++ src/containers/admin-page/filter.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled, { DefaultTheme } from 'styled-components';
+import Radio from '../../components/atoms/radio';
+import { exclusionCategory2Color } from '../../libs/exclusion-category-2-color';
+import { ExclusionCategory } from '../../models/exclusion-report';
+
+const Root = styled.form`
+	width: 272px;
+	background-color: white;
+`;
+
+const Title = styled.p`
+	font-size: 22px;
+	text-align: center;
+	color: black;
+	font-weight: bold;
+`;
+
+const RadioContainer = styled.div<{ colorSelector: (theme: DefaultTheme) => string }>`
+	background-color: ${props => props.colorSelector(props.theme)};
+	padding: 10px;
+	color: white;
+	margin: 1px 0;
+`;
+
+type FilterProps = React.PropsWithoutRef<{
+	options: readonly string[];
+	onChange?: (newValue: string) => void,
+}>;
+
+type FilterComponent = React.FunctionComponent<FilterProps>;
+
+const Filter: FilterComponent = ({
+	options,
+	onChange = () => {},
+}) => {
+	function  handleRadioChange (option: string) {
+		onChange(option);
+	}
+
+	return (
+		<Root>
+			<Title>Categorias</Title>
+			{options.map(option => (
+				<RadioContainer colorSelector={theme => exclusionCategory2Color(theme, option as ExclusionCategory) || theme.colors.primary.main}>
+					<Radio
+						name={option}
+						onChange={value => value && handleRadioChange(option)}
+					>
+						{option}
+					</Radio>
+				</RadioContainer>
+			))}
+		</Root>
+	);
+}
+
+export default Filter;
---
+++ src/containers/admin-page/index.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useLocalStorage } from '../../libs/hooks/use-local-storage';
+import { ExclusionCategory, ExclusionReport, ExclusionSource } from '../../models/exclusion-report';
+import Card from './card';
+import Filter from './filter';
+
+const Root = styled.div`
+	padding: 16px;
+	background-color: ${props => props.theme.colors.gray.light};
+	min-height: 100vh;
+	width: 100%;
+`;
+
+const Title = styled.h1`
+	font-size: 24px;
+	color: ${props => props.theme.colors.primary.main};
+	margin: 0;
+`;
+
+const Hr = styled.div`
+	width: 100%;
+	height: 1px;
+	border-bottom: 1px solid ${props => props.theme.colors.primary.main};
+	margin: 16px 0;
+`;
+
+const MetaInfo = styled.p`
+	margin: 8px 0;
+`;
+
+const MainContainer = styled.div`
+	width: 100%;
+	display: flex;
+	margin-top: 32px;
+`;
+
+const FilterContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-right: 16px;
+`;
+
+const CardsContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	width: 100%;
+	height: 100%;
+	justify-content: space-between;
+	row-gap: 32px;
+`;
+
+const FirstJobObservationText = styled.p`
+	color: ${props => props.theme.colors.primary.main};
+	font-size: 20px;
+`;
+
+type AdminPageComponent = React.FunctionComponent;
+
+const AdminPage: AdminPageComponent = ({  }) => {
+	const [categoryFilter, setCategoryFilter] = React.useState<string | null>(null);
+	const [sourceFilter, setSourceFilter] = React.useState<string | null>(null);
+
+	const [allReports] = useLocalStorage<ExclusionReport[]>('my-exclusion-reports', []);
+
+	const filteredReports = allReports.filter(report => {
+		if (categoryFilter && report.category !== categoryFilter) return false;
+		if (sourceFilter && report.source !== sourceFilter) return false;
+		return true;
+	});
+
+	return (
+		<Root>
+			<Title>Métricas com foco em inclusão da empresa Xis</Title>
+			<Hr />
+			<MetaInfo>
+				Período de Coleta: <strong>Jan, 2021</strong>
+			</MetaInfo>
+			<MetaInfo>
+				Recebemos <strong>322</strong> experiências, sendo <strong>150</strong> de
+				colaboradores no 1º emprego, estágio ou programa de treinee.
+			</MetaInfo>
+			<MainContainer>
+				<FilterContainer>
+					<Filter
+						options={ExclusionCategory}
+						onChange={setCategoryFilter}
+					/>
+					<Filter
+						options={ExclusionSource}
+						onChange={setSourceFilter}
+					/>
+					<FirstJobObservationText>
+						* Primeiro emprego, estágio ou programa de treinee.
+					</FirstJobObservationText>
+				</FilterContainer>
+				<CardsContainer>
+					{filteredReports.map(report => <Card report={report} />)}
+				</CardsContainer>
+			</MainContainer>
+		</Root>
+	);
+}
+
+export default AdminPage;
